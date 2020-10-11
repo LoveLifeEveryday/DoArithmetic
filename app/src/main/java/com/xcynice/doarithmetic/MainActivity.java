daec00a300501d02cf7f4bfec0d5439e0f4770ac
LoveLifeEveryday <913710642@qq.com>
2020-10-11 19:46:09 +0800

    feat(*):初始化项目

---
+++ app/src/main/java/com/xcynice/doarithmetic/MainActivity.java
@@ -0,0 +1,24 @@
+package com.xcynice.doarithmetic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @Author 许朋友爱玩
+ * @Date   2020/10/11
+ * @Github https://github.com/LoveLifeEveryday
+ * @JueJin https://juejin.im/user/5e429bbc5188254967066d1b/posts
+ *
+ * @Description MainActivity
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
